7d89f0b1e7e2a4a14138c253f4d212555a67973e
loicprodhom <54361074+loicprodhom@users.noreply.github.com>
2019-10-06 02:45:46 +0300

    Project Setup

---
+++ src/main/java/com/project/megusta/MegustaApplication.java
@@ -0,0 +1,13 @@
+package com.project.megusta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegustaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MegustaApplication.class, args);
+	}
+
+}
