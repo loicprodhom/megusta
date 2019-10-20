b026a68f1f83e94e6428f0370d8081a2f5dc3fdc
loicprodhom <54361074+loicprodhom@users.noreply.github.com>
2019-10-20 21:57:54 +0300

    test

--- src/main/java/com/project/megusta/tools/MeGustaTools.java
+++ src/main/java/com/project/megusta/tools/MeGustaTools.java
@@ -18,7 +18,8 @@ public class MeGustaTools {
         unitRepository.save(new Unit("Millilitre", "mL"));
         unitRepository.save(new Unit("Kilogram", "Kg"));
         unitRepository.save(new Unit("Gram", "g"));
-        unitRepository.save(new Unit("Teaspoon", "tsp"));
+        Unit teaspoon = new Unit("Teaspoon", "tsp");
+        unitRepository.save(teaspoon);
         unitRepository.save(new Unit("Tablespoon", "tbsp"));
         unitRepository.save(new Unit("Cup", "c"));
         unitRepository.save(new Unit("Unit", "piece(s)"));
@@ -37,8 +38,7 @@ public class MeGustaTools {
         Recipe pestoPasta = new Recipe("Pesto Pasta");
         recipeRepository.save(pestoPasta);
         List<Contains> contents = new ArrayList<Contains>();
-        // Contains pestoContent = new Contains(pesto, pestoPasta, 6, (Unit)
-        // unitRepository.findByName("Teaspoon"));
+        Contains pestoContent = new Contains(pesto, pestoPasta, 6.0, teaspoon);
         // contents.add(pestoContent);
         // Contains pastaContent = new
         // Contains((Ingredient) ingredientRepository.findByName("Spaghetti"), (Recipe)
