716e934a191f1ebe6d23b8bd541a901f7d9e2eec
loicprodhom <54361074+loicprodhom@users.noreply.github.com>
2019-10-20 21:11:37 +0300

    tests

--- src/main/java/com/project/megusta/tools/MeGustaTools.java
+++ src/main/java/com/project/megusta/tools/MeGustaTools.java
@@ -35,17 +35,17 @@ public class MeGustaTools {
         // Persisting the recipe "Pesto Pasta"
         Recipe pestoPasta = new Recipe("Pesto Pasta");
         recipeRepository.save(pestoPasta);
-        List<Contains> contents = new ArrayList<Contains>();
-        Contains pestoContent = new Contains((Ingredient) ingredientRepository.findByName("Pesto"),
-                (Recipe) recipeRepository.findByName("Pesto Pasta"), 6, (Unit) unitRepository.findByName("Teaspoon"));
-        contents.add(pestoContent);
-        containsRepository.save(pestoContent);
-        Contains pastaContent = new Contains((Ingredient) ingredientRepository.findByName("Spaghetti"),
-                (Recipe) recipeRepository.findByName("Pesto Pasta"), 500, (Unit) unitRepository.findByName("Gram"));
-        contents.add(pastaContent);
-        containsRepository.save(pastaContent);
-        pestoPasta.setIngredients(contents);
-        recipeRepository.save(pestoPasta);
+        // List<Contains> contents = new ArrayList<Contains>();
+        // Contains pestoContent = new
+        // Contains((Ingredient)ingredientRepository.findByName("Pesto"), (Recipe)
+        // recipeRepository.findByName("Pesto Pasta"), 6, (Unit)
+        // unitRepository.findByName("Teaspoon")); contents.add(pestoContent);
+        // containsRepository.save(pestoContent); Contains pastaContent = new
+        // Contains((Ingredient) ingredientRepository.findByName("Spaghetti"), (Recipe)
+        // recipeRepository.findByName("Pesto Pasta"), 500, (Unit)
+        // unitRepository.findByName("Gram")); contents.add(pastaContent);
+        // containsRepository.save(pastaContent); pestoPasta.setIngredients(contents);
+        // recipeRepository.save(pestoPasta);
 
     }
 }
