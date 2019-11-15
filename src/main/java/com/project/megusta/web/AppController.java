b4c8c690f90889b99a22a1ada40e189da0b828c7
loicprodhom <54361074+loicprodhom@users.noreply.github.com>
2019-11-15 14:33:55 +0200

    updated API endpoints

--- src/main/java/com/project/megusta/web/AppController.java
+++ src/main/java/com/project/megusta/web/AppController.java
@@ -3,7 +3,6 @@ package com.project.megusta.web;
 import java.util.List;
 
 import com.project.megusta.domain.ContainsRepository;
-import com.project.megusta.domain.Ingredient;
 import com.project.megusta.domain.IngredientRepository;
 import com.project.megusta.domain.Recipe;
 import com.project.megusta.domain.RecipeRepository;
@@ -12,7 +11,6 @@ import com.project.megusta.domain.UnitRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
